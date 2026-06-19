ea85cbf94968051bc3d9aee793f2b08053d14d0d
Kelvin <haminhhoang19@gmail.com>
2026-06-19 22:52:03 +1000

    Fixing tools bug

--- agent/tools.py
+++ agent/tools.py
@@ -1,4 +1,5 @@
-from langchain.agents import tool
+from langchain_core.tools import tool
+
 
 @tool
 def web_search(query: str) -> str:
@@ -6,3 +7,5 @@ def web_search(query: str) -> str:
     Search the live web for recent information on a given topic.
     """
     return "I'm sorry, I don't know how to do that."
+
+tools = [web_search]
